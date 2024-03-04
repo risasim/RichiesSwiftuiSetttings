09b5bb1cfc8cc0bd69b91455bfe7b182aa6f7807
Richie <richard1simonik@gmail.com>
2024-03-04 19:13:56 +0100

    added the app version

---
+++ Sources/RichiesSettings/AppVersionProvider.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Richard on 04.03.2024.
+//
+
+import Foundation
+
+enum AppVersionProvider {
+    static func appVersion(in bundle: Bundle = .main) -> String {
+        guard let version = bundle.object(forInfoDictionaryKey: "CFBundleShortVersionString") as? String else {
+            fatalError("CFBundleShortVersionString should not be missing from info dictionary")
+        }
+        return version
+    }
+}
--- Sources/RichiesSettings/SettingsFooterView.swift
+++ Sources/RichiesSettings/SettingsFooterView.swift
@@ -7,15 +7,17 @@ struct SettingsFooterView:View {
     
     var author:String
     var privacyPolicy:String?
+    var appVersion:String = AppVersionProvider.appVersion()
     
     var body: some View {
         HStack{
             Spacer()
             VStack{
-                Text(author)
+                Text("Version \(appVersion)")
                 if let pP = privacyPolicy, let url = URL(string: pP){
                     Link("Privacy Policy", destination: url)
                 }
+                Text(author)
             }
             .foregroundStyle(Color.gray)
             Spacer()
