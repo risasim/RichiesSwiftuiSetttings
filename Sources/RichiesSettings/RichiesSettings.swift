941f4864158eaf4e791c3d72c9ab1d7fcd871072
Richie <richard1simonik@gmail.com>
2024-03-04 15:43:23 +0100

    initialCommit

--- Sources/RichiesSettings/RichiesSettings.swift
+++ Sources/RichiesSettings/RichiesSettings.swift
@@ -1,2 +1,28 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+struct SettingsFooterView:View {
+    
+    var author:String
+    var privacyPolicy:String?
+    
+    var body: some View {
+        HStack{
+            Spacer()
+            VStack{
+                Text(author)
+                if let pP = privacyPolicy, let url = URL(string: pP){
+                    Link("Privacy Policy", destination: url)
+                }
+            }
+            .foregroundStyle(Color.gray)
+            Spacer()
+        }
+    }
+}
+
+#Preview{
+    SettingsFooterView(author: "Richard Simonik",privacyPolicy: "https://en.wikipedia.org/wiki/Wikipedia")
+}
